c4ad82ad60cac933c69281107ef2e4c1f3c04731
ANSCoder <nimjea@gmail.com>
2019-11-12 23:59:27 +0530

    Added LoadingViewController & Extensions.

---
+++ OlaApp-Task/Common/Extensions/ViewControllerExtension.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerExtension.swift
+//  OlaApp-Task
+//
+//  Created by Anand Nimje on 12/11/19.
+//  Copyright © 2019 Anand. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove() {
+        guard parent != nil else {
+            return
+        }
+        
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
